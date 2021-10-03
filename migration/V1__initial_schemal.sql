c914de33a9aa16938be5597380593029a6052c5e
Thuan Vo <thuanvip322@gmail.com>
2021-10-03 22:57:53 +0700

    Update DB

--- migration/V1__initial_schemal.sql
+++ migration/V1__initial_schemal.sql
@@ -47,13 +47,23 @@ DROP TABLE IF EXISTS `business_time`;
 CREATE TABLE `business_time` (
 	id int(10) NOT NULL AUTO_INCREMENT,
 	store_id int(10) NOT NULL,
-	start_time time NOT NULL,
-	end_time time NOT NULL,
-	day_of_week enum('MONDAY', 'THURSDAY', 'WEDNESDAY', 'TUESDAY', 'FRIDAY', 'SATURDAY', 'SUNDAY') NOT NULL,
+	start_time datetime NOT NULL,
+	end_time datetime NOT NULL,
+	day_of_week varchar(10) NOT NULL,
 	created_at timestamp NULL DEFAULT CURRENT_TIMESTAMP,
 	updated_at timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 	PRIMARY KEY (`id`,`store_id`)
 );
+INSERT INTO business_time (id, store_id, start_time, end_time, day_of_week, created_at, updated_at)
+VALUES ('1', '1','2021-09-24 07:00:00','2021-09-24 17:00:00', 'MONDAY','2021-09-24 09:16:08', '2021-09-24 09:16:08');
+INSERT INTO business_time (id, store_id, start_time, end_time, day_of_week, created_at, updated_at)
+VALUES ('2', '1','2021-09-24 07:00:00','2021-09-24 17:00:00', 'TUESDAY','2021-09-24 09:16:08', '2021-09-24 09:16:08');
+INSERT INTO business_time (id, store_id, start_time, end_time, day_of_week, created_at, updated_at)
+VALUES ('3', '1','2021-09-24 07:00:00','2021-09-24 17:00:00', 'WEDNESDAY','2021-09-24 09:16:08', '2021-09-24 09:16:08');
+INSERT INTO business_time (id, store_id, start_time, end_time, day_of_week, created_at, updated_at)
+VALUES ('4', '1','2021-09-24 07:00:00','2021-09-24 17:00:00', 'THURSDAY','2021-09-24 09:16:08', '2021-09-24 09:16:08');
+INSERT INTO business_time (id, store_id, start_time, end_time, day_of_week, created_at, updated_at)
+VALUES ('5', '1','2021-09-24 07:00:00','2021-09-24 17:00:00', 'FRIDAY','2021-09-24 09:16:08', '2021-09-24 09:16:08');
 
 DROP TABLE IF EXISTS `users`; 
 CREATE TABLE `users` (
@@ -75,6 +85,9 @@ CREATE TABLE `users` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
 );
+INSERT INTO users (id, fb_id , gg_id , email, password, salt, first_name, last_name, phone, avatar,lat,lng ,status, created_at, updated_at)
+VALUES ('1',null,null,'user@gmail.com','3d55a249c559fdd849bc80f07022d659','uEbHhwJMLSsxknTbvFGEqvygxtEUcqxGWUXAqeHeerxjGzPwEk',
+'user','user','0184632159',null,null,null,1,'2021-09-24 09:16:08', '2021-09-24 09:16:08');
 
 DROP TABLE IF EXISTS `clients`; 
 CREATE TABLE `clients` (
@@ -115,6 +128,12 @@ CREATE TABLE `staffs` (
   KEY `store_id` (`store_id`) USING BTREE,
   UNIQUE KEY `email` (`email`)
 );
+INSERT INTO staffs (id, store_id, first_name, last_name, phone, email, password, salt, role , avatar, status, created_at, updated_at)
+VALUES ('1', '1','admin','admin','0123456789','admin@gmail.com','3d55a249c559fdd849bc80f07022d659','uEbHhwJMLSsxknTbvFGEqvygxtEUcqxGWUXAqeHeerxjGzPwEk'
+,'owner',null,1,'2021-09-24 09:16:08', '2021-09-24 09:16:08');
+INSERT INTO staffs (id, store_id, first_name, last_name, phone, email, password, salt, role , avatar, status, created_at, updated_at)
+VALUES ('2', '1','staff','staff','0987654321','staff@gmail.com','04f6cbedf46e746614916262ff2eaa10','ZnGmaNTaCVTIgTQAYTBhoxlwhTwQiIDBtuDwrKHZUYYujNrVlm'
+,'staff',null,1,'2021-09-24 09:16:08', '2021-09-24 09:16:08');
 
 DROP TABLE IF EXISTS `services`; 
 CREATE TABLE `services` (
@@ -129,6 +148,13 @@ CREATE TABLE `services` (
 	PRIMARY KEY (`id`),
 	KEY `store_id` (`store_id`) USING BTREE
 );
+INSERT INTO services (id, store_id, category_id, title, description, status, created_at, updated_at)
+VALUES ('1', '1','1','Massage',null,1,'2021-09-24 09:16:08', '2021-09-24 09:16:08');
+INSERT INTO services (id, store_id, category_id, title, description, status, created_at, updated_at)
+VALUES ('2', '1','1','Lose Weight',null,1,'2021-09-24 09:16:08', '2021-09-24 09:16:08');
+INSERT INTO services (id, store_id, category_id, title, description, status, created_at, updated_at)
+VALUES ('3', '1','1','Facial Skin Care',null,1,'2021-09-24 09:16:08', '2021-09-24 09:16:08');
+
 
 DROP TABLE IF EXISTS `category`;
 CREATE TABLE `category` (
@@ -164,25 +190,27 @@ CREATE TABLE `service_staffs` (
 	PRIMARY KEY (`staff_id`,`service_id`)
 );
 
-DROP TABLE IF EXISTS `service_working`; 
-CREATE TABLE `service_working`  (
+DROP TABLE IF EXISTS `staff_working`; 
+CREATE TABLE `staff_working`  (
 	id int(10) NOT NULL,
 	staff_id int(10) NOT NULL,
-	start_time time NOT NULL,
-	end_time time NOT NULL,
-	working_date Date NOT NULL,
+	start_time datetime NOT NULL,
+	end_time datetime NOT NULL,
 	created_at timestamp NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 	PRIMARY KEY (`staff_id`,`id`)
 );
+INSERT INTO staff_working (id, staff_id, start_time, end_time,created_at,updated_at)
+VALUES ('1', '1', '2021-09-24 07:00:00', '2021-09-24 15:50:00','2021-09-24 09:16:08', '2021-09-24 09:16:08');
+INSERT INTO staff_working (id, staff_id, start_time, end_time ,created_at,updated_at)
+VALUES ('2', '2', '2021-09-24 09:00:00', '2021-09-24 09:50:00','2021-09-24 09:16:08', '2021-09-24 09:16:08');
 
-DROP TABLE IF EXISTS `service_working_repeat`; 
-CREATE TABLE `service_working_repeat` (
+DROP TABLE IF EXISTS `staff_working_repeat`; 
+CREATE TABLE `staff_working_repeat` (
 	id int(10) NOT NULL,
 	staff_id int(10) NOT NULL,
-	start_time time NOT NULL,
-	end_time time NOT NULL,
-	working_date Date NOT NULL,
+	start_time datetime NOT NULL,
+	end_time datetime NOT NULL,
 	specific_date Date NOT NULL,
 	created_at timestamp NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
@@ -203,6 +231,12 @@ CREATE TABLE `appointments` (
     updated_at timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 	PRIMARY KEY (`id`)
 );
+INSERT INTO appointments (id, user_id, store_id, staff_id, start_time, end_time, total_price,status,created_at,updated_at)
+VALUES ('1', '1', '1', '1', '2021-09-24 09:00:00', '2021-09-24 09:50:00', '200', '1', '2021-09-24 09:16:08', '2021-09-24 09:16:08');
+INSERT INTO appointments (id, user_id, store_id, staff_id, start_time, end_time, total_price,status,created_at,updated_at)
+VALUES ('2', '1', '1', '1', '2021-09-24 07:00:00', '2021-09-24 08:50:00', '200', '1', '2021-09-24 09:16:08', '2021-09-24 09:16:08');
+INSERT INTO appointments (id, user_id, store_id, staff_id, start_time, end_time, total_price,status,created_at,updated_at)
+VALUES ('3', '2', '1', '2', '2021-09-24 13:00:00', '2021-09-24 15:50:00', '200', '1', '2021-09-24 09:16:08', '2021-09-24 09:16:08');
 
 DROP TABLE IF EXISTS `appointment_service`; 
 CREATE TABLE `appointment_service`(
@@ -213,6 +247,18 @@ CREATE TABLE `appointment_service`(
     updated_at timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 	PRIMARY KEY (`appointment_id`,`service_id`)
 );
+INSERT INTO appointment_service (service_id,appointment_id,status,created_at,updated_at)
+VALUES (1, 1 , 1, '2021-09-24 09:16:08', '2021-09-24 09:16:08');
+INSERT INTO appointment_service (service_id,appointment_id,status,created_at,updated_at)
+VALUES (2, 1 , 1, '2021-09-24 09:16:08', '2021-09-24 09:16:08');
+INSERT INTO appointment_service (service_id,appointment_id,status,created_at,updated_at)
+VALUES (1, 2 , 1, '2021-09-24 09:16:08', '2021-09-24 09:16:08');
+INSERT INTO appointment_service (service_id,appointment_id,status,created_at,updated_at)
+VALUES (1, 3 , 1, '2021-09-24 09:16:08', '2021-09-24 09:16:08');
+INSERT INTO appointment_service (service_id,appointment_id,status,created_at,updated_at)
+VALUES (2, 3 , 1, '2021-09-24 09:16:08', '2021-09-24 09:16:08');
+INSERT INTO appointment_service (service_id,appointment_id,status,created_at,updated_at)
+VALUES (3, 3 , 1, '2021-09-24 09:16:08', '2021-09-24 09:16:08');
 
 DROP TABLE IF EXISTS `store_rating`; 
 CREATE TABLE `store_rating`(
@@ -226,3 +272,4 @@ CREATE TABLE `store_rating`(
 	PRIMARY KEY (`user_id`,`store_id`),
   KEY `store_id` (`store_id`)
 );
+
