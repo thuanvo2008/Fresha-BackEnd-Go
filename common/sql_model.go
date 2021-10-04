2d6aac84a07b81b0eccb31f0d9f889959aef015c
Thuan Vo <thuanvip322@gmail.com>
2021-10-04 20:25:16 +0700

    remove fakeId

--- common/sql_model.go
+++ common/sql_model.go
@@ -5,8 +5,8 @@ import (
 )
 
 type SQLModel struct {
-	Id        int        `json:"-" gorm:"column:id;"`
-	FakeID    *UID       `json:"id" gorm:"-"`
+	Id        int        `json:"id" gorm:"column:id;"`
+	FakeID    *UID       `json:"-" gorm:"-"`
 	Status    int        `json:"status" gorm:"column:status;default:1;"`
 	CreatedAt *time.Time `json:"created_at" gorm:"created_at"`
 	UpdatedAt *time.Time `json:"updated_at" gorm:"updated_at"`
